9255586f1e3e339b493317b5ffd8c017db217e79
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2024-01-17 23:39:05 +0600

    NavBar Added

--- client/components/layout/NavBar.tsx
+++ client/components/layout/NavBar.tsx
@@ -2,11 +2,17 @@
 
 import React from "react";
 import { UserButton } from "@clerk/nextjs";
+import Container from "../Container";
 
 const NavBar = () => {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
+    <div
+      className="sticky top-0 border 
+     bg-secondary "
+    >
+      <Container>
+        <UserButton afterSignOutUrl="" />
+      </Container>
     </div>
   );
 };
